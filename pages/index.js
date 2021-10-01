3681e252022d5f3ac9c6ea67bc42206cc8d78f87
B-12 <aabduvakhobov02@gmail.com>
2021-10-01 22:19:45 +0500

    Added Small Card part of main section, used Static rendering to display images and infos of small cards

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import SmallCard from "../components/SmallCard";
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div>
       <Head>
@@ -14,6 +15,33 @@ export default function Home() {
       </Head>
       <Header />
       <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-bold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, location, distance }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                location={location}
+                distance={distance}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://jsonkeeper.com/b/4G1G").then((res) =>
+    res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
